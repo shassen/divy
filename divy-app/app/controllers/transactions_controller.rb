2dce81f341bdd7e0947ec1e97bb3492ebccbefcb
Shawn Hassen <Shassen89@gmail.com>
2018-09-07 10:47:08 -0400

    Full CRUD on transaction table complete, still working on register for users and CRUD on transaction_users table

--- divy-app/app/controllers/transactions_controller.rb
+++ divy-app/app/controllers/transactions_controller.rb
@@ -1,35 +1,49 @@
 class TransactionsController < ApplicationController
 
+    # INDEX users/:id/transactions - show all transactions for user
     def index
         @txns = Transaction.all
         render json: { txn: @txns }
     end
 
+    # SHOW users/:id/transactions/:id - show one transaction
     def show
         @txn = Transaction.find(params[:id])
         render json: { txn: @txn }
     end
 
+    # CREATE users/:id/transactions - create transactions
+    # Conditional stmt to create a row on the transaction_users table
     def create
-        @group ? @group.transactions.create!(txn_params) : Transaction.create(txn_params)
+        # @group ? @group.transactions.create!(txn_params) : Transaction.create(txn_params)
         # @new_txn = Transaction.new(txn_params)
         render json: { txn: "Transaction created" }
     end
 
+    # UPDATE users/:id/transactions/:id - update a transaction
     def update
         @update_txn = Transaction.find(params[:id]).update(txn_params)
         render json: { txn: @update_txn }
     end
 
+    # DELETE users/:id/transactions/:id - destroy transactions
     def destroy
-        render json: { message: "I delete transactions" }
+        @txn = Transaction.find(params[:id])
+        @txn.destroy
+        render json: { message: "Transaction #{params[:id]} has been destroyed" }
     end
 
     private
 
-    def set_group
-        if params[:group_id]
-            @group = Group.find(params[:group_id])
+    # def set_group
+    #     if params[:group_id]
+    #         @group = Group.find(params[:group_id])
+    #     end
+    # end
+
+    def set_user
+        if params[:user_id]
+            @user = User.find(params[:id])
         end
     end
 
--- divy-app/config/routes.rb
+++ divy-app/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
-  resources :groups do
-    resources :transactions
-    end
+  # resources :groups do
+  #   resources :transactions
+  #   end
   resources :users do
     resources :transactions
   end
