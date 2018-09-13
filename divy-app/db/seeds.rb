7741c4272470a392f35ba6147f8698c5db45d92f
Shawn Hassen <Shassen89@gmail.com>
2018-09-13 18:23:07 -0400

    adjusted seed

--- divy-app/db/seeds.rb
+++ divy-app/db/seeds.rb
@@ -5,35 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.first
+u2 = User.find(14);
+u3 = User.find(15);
+t1 = Transaction.first;
+t2 = Transaction.find(66);
+t3 = Transaction.find(67);
+t4 = Transaction.find(68);
+t5 = Transaction.find(69);
 
-User.delete_all
-Transaction.delete_all
-Group.delete_all
-GroupUser.delete_all
+# User.delete_all
+# Transaction.delete_all
+# Group.delete_all
+# GroupUser.delete_all
 
-User.create!([
-    {first_name: "test", last_name: "test", username: "testy", wallet: 14000, email: "testy@test.com", password: "test", password_confirmation: "test", admin: true},
-    {first_name: "admin", last_name: "admin", username: "admin", wallet: 6000, email: "admin@admin.com", password: "admin", password_confirmation: "admin", admin: true},
-    {first_name: "user1", last_name: "user1", username: "user1", wallet: 5000, email: "user1@user1.com", password: "user1", password_confirmation: "user1", admin: true}
-])
+# User.create!([
+#     {first_name: "test", last_name: "test", username: "testy", wallet: 14000, email: "testy@test.com", password: "test", password_confirmation: "test", admin: true},
+#     {first_name: "admin", last_name: "admin", username: "admin", wallet: 6000, email: "admin@admin.com", password: "admin", password_confirmation: "admin", admin: true},
+#     {first_name: "user1", last_name: "user1", username: "user1", wallet: 5000, email: "user1@user1.com", password: "user1", password_confirmation: "user1", admin: true}
+# ])
 
-Group.create!([
-    {name: "Home", wallet: 700, description: "Share all purchases between us"}
-])
+# Group.create!([
+#     {name: "Home", wallet: 700, description: "Share all purchases between us"}
+# ])
 
-Transaction.create!([
-    {amount: 350, location: "BestBuy", description: "Purchased a wireless router for apartment", approved: true, group: Group.first},
-    {amount: 500, location: "Amazon.com", description: "New parts for PC", approved: true, group: Group.first},
-    {amount: 1000, location: "Crate & Barrel", description: "New side table for apartment", approved: true, group: Group.first},
-    {amount: 1500, location: "Crate & Barrel", description: "New couch for apartment", approved: true, group: Group.first},
-    {amount: 80, location: "Amazon.com", description: "Cleaning supplies for apartment", approved: true, group: Group.first}
-])
+# Transaction.create!([
+#     {amount: 350, location: "BestBuy", description: "Purchased a wireless router for apartment", approved: true, group: Group.first},
+#     {amount: 500, location: "Amazon.com", description: "New parts for PC", approved: true, group: Group.first},
+#     {amount: 1000, location: "Crate & Barrel", description: "New side table for apartment", approved: true, group: Group.first},
+#     {amount: 1500, location: "Crate & Barrel", description: "New couch for apartment", approved: true, group: Group.first},
+#     {amount: 80, location: "Amazon.com", description: "Cleaning supplies for apartment", approved: true, group: Group.first}
+# ])
 
-GroupUser.create!([
-    {user: User.first, group: Group.first, admin: true, approved: false}
-])
+# GroupUser.create!([
+#     {user: User.first, group: Group.first, admin: true, approved: false}
+# ])
 
 TransactionUser.create!([
-    {user: User.first, txn: Transaction.first, admin: true, approved: false},
+    {user: u1, txn: t1, admin: true, approved: false},
+    {user: u1, txn: t2, admin: true, approved: false},
+    {user: u1, txn: t3, admin: true, approved: false},
+    {user: u1, txn: t4, admin: true, approved: false},
+    {user: u1, txn: t5, admin: true, approved: false},
+    {user: u2, txn: t2, admin: true, approved: false},
+    {user: u2, txn: t3, admin: true, approved: false},
+    {user: u3, txn: t1, admin: true, approved: false},
+    {user: u3, txn: t2, admin: true, approved: false}
 ])
 
