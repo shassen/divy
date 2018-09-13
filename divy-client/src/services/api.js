70f7a5c400d6f07f077af0ceb19f2da60332f608
Shawn Hassen <Shassen89@gmail.com>
2018-09-13 17:49:46 -0400

    made localhost active

--- divy-client/src/services/api.js
+++ divy-client/src/services/api.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'http://localhost:3000'
-const BASE_URL = process.env.REACT_APP_BASE_URL
+const BASE_URL = 'http://localhost:3000'
+// const BASE_URL = process.env.REACT_APP_BASE_URL
 
 
 // Function: gets current users transactions from DB
