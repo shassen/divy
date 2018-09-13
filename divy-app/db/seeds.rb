47d10b8c76466239e1720387c2069d1cf276fc82
Shawn Hassen <Shassen89@gmail.com>
2018-09-13 18:06:26 -0400

    updated seed file

--- divy-app/db/seeds.rb
+++ divy-app/db/seeds.rb
@@ -12,7 +12,9 @@ Group.delete_all
 GroupUser.delete_all
 
 User.create!([
-    {first_name: "test", last_name: "test", username: "testy", wallet: 100, email: "testy@test.com", password: "test", password_confirmation: "test", admin: true}
+    {first_name: "test", last_name: "test", username: "testy", wallet: 14000, email: "testy@test.com", password: "test", password_confirmation: "test", admin: true},
+    {first_name: "admin", last_name: "admin", username: "admin", wallet: 6000, email: "admin@admin.com", password: "admin", password_confirmation: "admin", admin: true},
+    {first_name: "user1", last_name: "user1", username: "user1", wallet: 5000, email: "user1@user1.com", password: "user1", password_confirmation: "user1", admin: true}
 ])
 
 Group.create!([
@@ -20,7 +22,11 @@ Group.create!([
 ])
 
 Transaction.create!([
-    {amount: 350, location: "BestBuy", description: "Purchased a wireless router for apartment", approved: true, group: Group.first}
+    {amount: 350, location: "BestBuy", description: "Purchased a wireless router for apartment", approved: true, group: Group.first},
+    {amount: 500, location: "Amazon.com", description: "New parts for PC", approved: true, group: Group.first},
+    {amount: 1000, location: "Crate & Barrel", description: "New side table for apartment", approved: true, group: Group.first},
+    {amount: 1500, location: "Crate & Barrel", description: "New couch for apartment", approved: true, group: Group.first},
+    {amount: 80, location: "Amazon.com", description: "Cleaning supplies for apartment", approved: true, group: Group.first}
 ])
 
 GroupUser.create!([
@@ -28,6 +34,6 @@ GroupUser.create!([
 ])
 
 TransactionUser.create!([
-    {user: User.first, txn: Transaction.first, admin: true, approved: false}
+    {user: User.first, txn: Transaction.first, admin: true, approved: false},
 ])
 
